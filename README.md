9c4b1a64ff3c18abe23d064b850271abde99a5e8
Kacper Adamus <kacper.adamus@zorius.pl>
2024-10-31 09:32:20 +0100

    TodayButtonSeD

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Skrypty
+
+## TodayButtonSeD [Plik](./TodayButtonSeD.js)
+Dodaje przycisk Dzisiaj do SoD'a, który automatycznie ustawia dzisiejszą datę
---
+++ TodayButtonSeD.js
@@ -0,0 +1,97 @@
+// ==UserScript==
+// @name         Today Button
+// @namespace    http://tampermonkey.net/
+// @version      2024-10-30
+// @description  Adding today button to SeD work time
+// @author       You
+// @match        https://351509.edokumenty.symfonia.pl/apps/edokumenty/engine/351509/Welcome
+// @match        edokumenty.symfonia.pl
+// @match        https://351509.edokumenty.symfonia.pl/apps/edokumenty/engine
+// @match        https://351509.edokumenty.symfonia.pl/apps/edokumenty/
+// @match        https://351509.edokumenty.symfonia.pl/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=symfonia.pl
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let date
+    let alreadyAdded = []
+    let startInput, endInput, dialogTodo
+
+    const button = document.createElement('button')
+    button.className = "rbos-btn rbos-btn-tertiary rbos-btn-cancel";
+    button.style.cursor = "pointer"
+    button.innerHTML = "Dzisiaj"
+
+    button.onclick = (e) => {
+        e.preventDefault()
+        const today = new Date()
+        date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate()
+        startInput.value = date
+        endInput.value = date
+    }
+
+    let currentId = ""
+    let cActId = ""
+
+    const addButton = (id) => {
+        const element = document.querySelector("#"+id+"bpanel");
+
+        if (id.includes("RCP")) {
+            element.prepend(button)
+            alreadyAdded.push(dialogTodo)
+        }
+
+        startInput = document.querySelector("#"+id+"rlstrtd")
+        endInput = document.querySelector("#"+id+"rlend_d")
+
+
+        const today = new Date()
+        date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate()
+        startInput.value = date
+        endInput.value = date
+    }
+
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach(mutation => {
+            mutation.removedNodes.forEach(node => {
+                if (node.nodeType === 1 && node.classList.contains('c-act')) {
+                    alreadyAdded = [];
+                }
+            });
+        });
+
+        const dialogs = document.querySelectorAll(".Dialog")
+        const cAct = document.querySelectorAll(".c-act")
+
+        dialogs.forEach(dialog => dialog.removeEventListener("click", dialogListener))
+        cAct.forEach(item => item.removeEventListener("click", cActListener))
+
+        dialogs.forEach(dialog => dialogListener(dialog))
+        cAct.forEach(item => cActListener(item))
+    })
+
+    const dialogListener = (dialog) => {
+        dialog.onclick = () => {
+            currentId = dialog.attributes.id.value
+
+            if (currentId.includes("TODOdlg")) {
+                dialogTodo = currentId
+            }
+        }
+    }
+
+    const cActListener = (item) => {
+        cActId = item.attributes.edappref.value
+        if (cActId === currentId && !alreadyAdded.includes(dialogTodo) && cActId.includes("RCP")) {
+            addButton(cActId)
+        }
+    }
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    })
+})();
+
